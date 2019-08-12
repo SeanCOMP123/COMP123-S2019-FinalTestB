e577b8a0cc24bcbd9f9142d66a28c2e4cea5a198
Myoung Seop Kwak <mkwak@my.centennialcollege.ca>
2019-08-12 13:05:50 -0400

    added data files

---
+++ COMP123-S2019-FinalTestB/Objects/CharacterClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Student Name: Sean Kwak
+ * Student ID: 300614352
+ * Description : This is an enum for the Character Class
+ */
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    public enum CharacterClass
+    {
+        FIGHTER,
+        ROGUE,
+        WIZARD,
+        CLERIC,
+        RANGER,
+        MONK,
+        BARBARIAN,
+        PALADIN,
+        BARD,
+        WARLOCK,
+        SORCEROR,
+        DRUID
+    }
+}
